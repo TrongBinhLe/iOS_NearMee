c957c546ff23437006926e63fc244c3214841e2f
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-05-16 16:55:29 +0700

    featur: Create a new project is named NearMee
    Dont' use storyboard to create the project. Adding more Mapkit to get local location

---
+++ NearMee/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  NearMee
+//
+//  Created by admin on 16/05/2023.
+//
+
+import UIKit
+ import MapKit
+
+class ViewController: UIViewController {
+    
+    lazy var mapView: MKMapView = {
+       let map = MKMapView()
+        map.showsUserLocation = true
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+    
+    lazy var searchTextField: UITextField = {
+        let searchTextField  = UITextField()
+        searchTextField.layer.cornerRadius = 10
+        searchTextField.clipsToBounds = true
+        searchTextField.backgroundColor = .white
+        searchTextField.placeholder = "Search"
+        searchTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        searchTextField.leftViewMode = .always
+        searchTextField.returnKeyType = .go
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+
+        return searchTextField
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.green
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.addSubview(searchTextField)
+        view.addSubview(mapView)
+        
+        view.bringSubviewToFront(searchTextField)
+        
+        //add contraints to the search text field
+        NSLayoutConstraint.activate([
+            searchTextField.heightAnchor.constraint(equalToConstant: 44),
+            searchTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            searchTextField.widthAnchor.constraint(equalToConstant: view.bounds.size.width/1.2),
+            searchTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 60)
+        ])
+        
+        //add contraints to the mapView
+        NSLayoutConstraint.activate([
+            mapView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            mapView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+}
+
